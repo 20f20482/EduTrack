af3f7ebdace277d4e228390ed779f636ef6aea40
20f20482 <148860546+20f20482@users.noreply.github.com>
2025-06-21 12:11:29 +0400

    Update HomeActivity.java

--- app/src/main/java/com/example/edutrack/HomeActivity.java
+++ app/src/main/java/com/example/edutrack/HomeActivity.java
@@ -33,8 +33,8 @@ public class HomeActivity extends AppCompatActivity {
             return ;
         }
 
-        Toolbar topToolbar482 = findViewById(R.id.topToolbar);
-        Toolbar bottomToolbar482 = findViewById(R.id.bottomToolbar);
+        Toolbar topToolbar482 = findViewById(R.id.topToolbar482);
+        Toolbar bottomToolbar482 = findViewById(R.id.bottomToolbar482);
         SearchView searchView482 = findViewById(R.id.searchView482);
 
         CardView cardPathways = findViewById(R.id.cardPathways482);
